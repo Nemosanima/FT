046519bb14dc0386f8e140029e6312412a9ab16c
Rasul-code <sadlarkhanov-rasul@mail.ru>
2023-05-19 16:25:54 +0400

    Tried WTF Forms

--- src/app.py
+++ src/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+
+# WTF Form
+class NamerForm(FlaskForm):
+    name = StringField("Имя", validators=[DataRequired()])
+    submit = SubmitField("Отправить")
 
 
 @app.route('/')
@@ -12,6 +21,21 @@ def index():
 @app.errorhandler(404)
 def error_404(error):
     return render_template("errors/404.html"), 404
+
+
+# Test WTF Form
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("app/name.html",
+                           name=name,
+                           form=form
+    )
 
 
 if __name__ == "__main__":
