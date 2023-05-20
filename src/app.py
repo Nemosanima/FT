8de11f7f3587deca8f877accbd811dcd88c9769a
Rasul-code <sadlarkhanov-rasul@mail.ru>
2023-05-20 10:42:26 +0400

    testing migrations

--- src/app.py
+++ src/app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 # Create flask app
 app = Flask(__name__)
@@ -15,6 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
 # Создаем базу данных
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # User model
@@ -23,6 +26,7 @@ class User(db.Model):
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     created = db.Column(db.DateTime, default=datetime.utcnow)
+    favorite_color = db.Column(db.String(50))
 
     def __repr__(self):
         return '<Name %r>' % self.name
@@ -32,6 +36,7 @@ class User(db.Model):
 class UserForm(FlaskForm):
     name = StringField("Имя", validators=[DataRequired()])
     email = StringField("Почта", validators=[DataRequired()])
+    favorite_color = StringField("Любимый цвет")
     submit = SubmitField("Отправить")
 
 
@@ -73,11 +78,12 @@ def add_user():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = User(name=form.name.data, email=form.email.data)
+            user = User(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         form.name.data = ''
         form.email.data = ''
+        form.favorite_color.data = ''
         flash('Пользователь добавлен в базу данных успешно')
     users = User.query.order_by(User.created)
     return render_template("users/add_user.html",
@@ -92,6 +98,7 @@ def update_user(id):
     if request.method == 'POST':
         user.name = request.form.get('name')
         user.email = request.form.get('email')
+        user.favorite_color = request.form.get('favorite_color')
         try:
             db.session.commit()
             flash('Данные пользователя обновлены')
